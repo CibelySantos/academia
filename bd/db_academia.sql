9bd5a32bbc196f8f1898ac393d01af4554dc1ff7
juliafortunat24 <juliafortunat24@gmail.com>
2025-01-30 13:38:32 -0300

    commit banco

--- bd/db_academia.sql
+++ bd/db_academia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 30-Jan-2025 às 15:22
+-- Tempo de geração: 30-Jan-2025 às 17:37
 -- Versão do servidor: 10.4.27-MariaDB
 -- versão do PHP: 8.2.0
 
@@ -31,9 +31,21 @@ CREATE TABLE `aluno` (
   `aluno_cod` int(11) NOT NULL,
   `aluno_nome` text NOT NULL,
   `aluno_cpf` varchar(20) NOT NULL,
-  `aluno_endereco` varchar(500) NOT NULL,
-  `fk_aluno_telefone` int(11) NOT NULL
+  `aluno_endereco` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `aluno`
+--
+
+INSERT INTO `aluno` (`aluno_cod`, `aluno_nome`, `aluno_cpf`, `aluno_endereco`) VALUES
+(5, 'julia', '30445', 'Rua jardim'),
+(6, 'Luiz', '33322', 'Rua Longe'),
+(7, 'Cibely', '88844', 'Rua Dois'),
+(8, 'Ana Maria', '65667', 'Rua da luz'),
+(9, 'Catarina', '222233', 'Rua Dois'),
+(10, 'Claudio', '556677', 'Rua dos banzeiros'),
+(11, 'Flavio', '77456', 'Avenida Brasil');
 
 -- --------------------------------------------------------
 
@@ -48,6 +60,19 @@ CREATE TABLE `aula` (
   `fk_instrutor_cod` int(11) NOT NULL,
   `fk_aluno_cod` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `aula`
+--
+
+INSERT INTO `aula` (`aula_cod`, `aula_tipo`, `aula_data`, `fk_instrutor_cod`, `fk_aluno_cod`) VALUES
+(1, 'zumba', '2025-01-30 15:42:48', 1, 10),
+(2, 'natação', '2025-01-30 15:44:17', 2, 11),
+(3, 'Crossfit', '2025-01-30 15:44:42', 3, 9),
+(4, 'musculação', '2025-01-30 15:45:05', 4, 6),
+(5, 'yoga', '2025-01-30 15:45:39', 5, 8),
+(6, 'Pilates', '2025-01-30 15:46:12', 6, 8),
+(7, 'Boxe', '2025-01-30 15:46:27', 7, 7);
 
 -- --------------------------------------------------------
 
@@ -66,7 +91,13 @@ CREATE TABLE `instrutores` (
 --
 
 INSERT INTO `instrutores` (`instrutor_cod`, `instrutor_nome`, `instrutor_especialidade`) VALUES
-(1, 'amanda', 'zumba');
+(1, 'amanda', 'zumba'),
+(2, 'Beatriz', 'Natação'),
+(3, 'Pedro', 'Crossfit'),
+(4, 'Mario', 'Musculação'),
+(5, 'Júlia', 'Yoga'),
+(6, 'Roberto', 'pilates'),
+(7, 'Adriano', 'Boxe');
 
 -- --------------------------------------------------------
 
@@ -81,6 +112,19 @@ CREATE TABLE `telefone` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Extraindo dados da tabela `telefone`
+--
+
+INSERT INTO `telefone` (`telefone_cod`, `fk_aluno_cod`, `telefone`) VALUES
+(2, 5, '888888'),
+(3, 6, '999999'),
+(4, 7, '777777'),
+(5, 8, '666666'),
+(6, 9, '666666'),
+(7, 10, '555555'),
+(8, 11, '444444');
+
+--
 -- Índices para tabelas despejadas
 --
 
@@ -88,8 +132,7 @@ CREATE TABLE `telefone` (
 -- Índices para tabela `aluno`
 --
 ALTER TABLE `aluno`
-  ADD PRIMARY KEY (`aluno_cod`),
-  ADD KEY `fk_aluno_telefone` (`fk_aluno_telefone`);
+  ADD PRIMARY KEY (`aluno_cod`);
 
 --
 -- Índices para tabela `aula`
@@ -120,35 +163,29 @@ ALTER TABLE `telefone`
 -- AUTO_INCREMENT de tabela `aluno`
 --
 ALTER TABLE `aluno`
-  MODIFY `aluno_cod` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `aluno_cod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de tabela `aula`
 --
 ALTER TABLE `aula`
-  MODIFY `aula_cod` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `aula_cod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de tabela `instrutores`
 --
 ALTER TABLE `instrutores`
-  MODIFY `instrutor_cod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `instrutor_cod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de tabela `telefone`
 --
 ALTER TABLE `telefone`
-  MODIFY `telefone_cod` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `telefone_cod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Restrições para despejos de tabelas
 --
-
---
--- Limitadores para a tabela `aluno`
---
-ALTER TABLE `aluno`
-  ADD CONSTRAINT `aluno_ibfk_1` FOREIGN KEY (`fk_aluno_telefone`) REFERENCES `telefone` (`telefone_cod`);
 
 --
 -- Limitadores para a tabela `aula`
