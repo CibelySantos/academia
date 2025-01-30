aa1d70c08606bed441c3e3217bfa07eedd935064
LuizGustavo007 <luizinhogucpv@gmail.com>
2025-01-30 13:35:02 -0300

    commit luiz

--- edicao_instrutor.php
+++ edicao_instrutor.php
@@ -66,7 +66,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['atualizar'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar Instrutor</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="./css/edicao.css">
 </head>
 <body>
 <div class="container mt-4">
